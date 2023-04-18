1f75961f5a11a01cf35acaae3818e456822e8aa4
Savon Huynh <xqhuynh@gmail.com>
2023-04-17 22:09:53 -0700

    JS dark light mode and scroll reveal

--- js/script.js
+++ js/script.js
@@ -52,3 +52,30 @@ var swiper = new Swiper('.mySwiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+// Dark light mode
+let darkModeIcon = document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick = () => {
+  darkModeIcon.classList.toggle('bx-sun');
+  document.body.classList.toggle('dark-mode');
+};
+
+// Scroll reveal
+
+ScrollReveal({
+  // reset: true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal(
+  '.home-img img, .services-container, .portfolio-box, .testimonial-wrapper, .contact form',
+  { origin: 'bottom' }
+);
+ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
+ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', {
+  origin: 'right',
+});
